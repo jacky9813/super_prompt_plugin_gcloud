0f1086a345053c3dfdd6ba3fed0107c1d1233a75
Jacky Chen <jacky.chen@microfusion.cloud>
2023-08-17 09:29:40 +0800

    fix: unable to load super_prompt config

--- super_prompt_plugin_gcloud/main.py
+++ super_prompt_plugin_gcloud/main.py
@@ -36,7 +36,7 @@ def main(config: dict) -> typing.Optional[super_prompt.types.PluginResponse]:
     if not os.path.exists(ACTIVE_CONFIG_FILE):
         logger.info("Path %s does not exist", ACTIVE_CONFIG_FILE)
         return
-    
+
     with open(ACTIVE_CONFIG_FILE) as active_config_fd:
         active_config = active_config_fd.read().strip()
 
@@ -45,20 +45,20 @@ def main(config: dict) -> typing.Optional[super_prompt.types.PluginResponse]:
     if not os.path.exists(CONFIG_LOCATION_TEMPLATE.format(active_config)):
         logger.info("Path %s does not exist", os.path.exists(CONFIG_LOCATION_TEMPLATE.format(active_config)))
         return
-    
-    config = configparser.ConfigParser()
-    config.read(CONFIG_LOCATION_TEMPLATE.format(active_config))
-    current_project = config.get("core", "project", fallback=None)
+
+    gcloud_config = configparser.ConfigParser()
+    gcloud_config.read(CONFIG_LOCATION_TEMPLATE.format(active_config))
+    current_project = gcloud_config.get("core", "project", fallback=None)
 
     if current_project is None:
         return
-    
+
     if not os.path.exists(ADC_CREDENTIAL_FILE):
         return super_prompt.types.PluginResponse("G", current_project, COLOR_ADC_INVALID)
     adc_too_old = isodate.parse_duration(config.get("adc_notice_time", "PT8H"))
     adc_age = datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(os.stat(ADC_CREDENTIAL_FILE).st_mtime)
     return super_prompt.types.PluginResponse("G", current_project, COLOR_ADC_INVALID if adc_age > adc_too_old else None)
-    
+
 
 if __name__ == "__main__":
     print(main({}))
